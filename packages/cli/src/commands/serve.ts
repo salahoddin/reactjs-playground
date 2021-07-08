fbc36ad9426a90e805a9a3ff127c295fddf17e27
salahoddin <salahoddinsambutuan@gmail.com>
2021-07-08 09:39:14 +0800

    deployed to npm

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -8,9 +8,12 @@ export const serveCommand = new Command()
 	.command('serve [filename]')
 	.description('Open a file for editing')
 	.option('-p, --port <number>', 'port to run server on', '4005')
+	// receive the values from CLI. 1st arg is from root command, 2nd is an objects from option
 	.action(async (filename = 'notebook.js', options: { port: string }) => {
 		try {
 			const dir = path.join(process.cwd(), path.dirname(filename))
+
+			// made serve function return a promise, see local-api/src/index
 			await serve(
 				parseInt(options.port),
 				path.basename(filename),
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import { serveCommand } from './commands/serve';
+import { program } from 'commander'
+import { serveCommand } from './commands/serve'
 
-program.addCommand(serveCommand);
+program.addCommand(serveCommand)
 
-program.parse(process.argv);
+program.parse(process.argv)
